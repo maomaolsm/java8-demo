109a00ae3a9420adb44ee54c5c8b7a4276b0ecf9
maomao <maomao.li@jjcargo.com>
2016-10-11 13:26:29 +0800

    first

--- src/JavaStream.java
+++ src/JavaStream.java
@@ -1,24 +1,33 @@
 import java.util.Arrays;
 import java.util.List;
 
+import static java.util.stream.Collectors.toList;
+
 /**
  * Created by maomao on 16/10/11.
  */
 public class JavaStream {
-
-    List<Dish> menu = Arrays.asList(
-        new Dish("pork", false, 800, Dish.Type.MEAT),
-        new Dish("beef", false, 700, Dish.Type.MEAT),
-        new Dish("chicken", false, 400, Dish.Type.MEAT),
-        new Dish("french", false, 530, Dish.Type.OTHER),
-        new Dish("rice", true, 350, Dish.Type.OTHER),
-        new Dish("season fruit", true, 120, Dish.Type.OTHER),
-        new Dish("pizza", true, 550, Dish.Type.OTHER),
-        new Dish("prawns", false, 300, Dish.Type.FISH),
-        new Dish("salmon", false, 450, Dish.Type.FISH)
-    );
-
     public static void main(String[] args) {
 
+        List<Dish> menu = Arrays.asList(
+            new Dish("pork", false, 800, Dish.Type.MEAT),
+            new Dish("beef", false, 700, Dish.Type.MEAT),
+            new Dish("chicken", false, 400, Dish.Type.MEAT),
+            new Dish("french", false, 530, Dish.Type.OTHER),
+            new Dish("rice", true, 350, Dish.Type.OTHER),
+            new Dish("season fruit", true, 120, Dish.Type.OTHER),
+            new Dish("pizza", true, 550, Dish.Type.OTHER),
+            new Dish("prawns", false, 300, Dish.Type.FISH),
+            new Dish("salmon", false, 450, Dish.Type.FISH)
+        );
+
+
+        List<String> threeHighCaloricDishNames =
+            menu.stream()
+                .filter(d -> d.getCalories() > 300)
+                .map(Dish::getName)
+                .limit(3)
+                .collect(toList());
+        System.out.println(threeHighCaloricDishNames);
     }
 }
