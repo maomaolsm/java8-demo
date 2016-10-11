3ca3721ce5d14237ebff2c7496fe1fd7fe4753d9
maomao <maomao.li@jjcargo.com>
2016-10-11 12:05:59 +0800

    init

---
+++ src/JavaStream.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by maomao on 16/10/11.
+ */
+public class JavaStream {
+
+    List<Dish> menu = Arrays.asList(
+        new Dish("pork", false, 800, Dish.Type.MEAT),
+        new Dish("beef", false, 700, Dish.Type.MEAT),
+        new Dish("chicken", false, 400, Dish.Type.MEAT),
+        new Dish("french", false, 530, Dish.Type.OTHER),
+        new Dish("rice", true, 350, Dish.Type.OTHER),
+        new Dish("season fruit", true, 120, Dish.Type.OTHER),
+        new Dish("pizza", true, 550, Dish.Type.OTHER),
+        new Dish("prawns", false, 300, Dish.Type.FISH),
+        new Dish("salmon", false, 450, Dish.Type.FISH)
+    );
+
+    public static void main(String[] args) {
+
+    }
+}
